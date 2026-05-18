0e37575c8b2838fa90dacf5432079c4102b19798
Jaynikhar <jaynikhar61@gmail.com>
2026-05-18 19:33:09 +0530

    add frontend link

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://team-task-management-1wbukv58m-jay-nikhars-projects.vercel.app/signup",
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
